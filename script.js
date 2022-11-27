d8903c9518b9bcca8743cebd8e1bf181a836a7e3
olafnub <googlhoogle@gmail.com>
2022-11-27 12:41:47 -0600

    Debugging localStorage
    **Learning how .getItem and .setItem works

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-let diaries = [];
+// let diaries = [];
     
-const addDiary = (ev)=>{
-ev.preventDefault();
-    let diary = {
-    id: Date(Date.now()),
-    titles: document.getElementById("diaryTitle").value,
-    paragraphs: document.getElementById("txt").value
+// const addDiary = (ev)=>{
+// ev.preventDefault();
+//     let diary = {
+//     id: Date(Date.now()),
+//     titles: document.getElementById("diaryTitle").value,
+//     paragraphs: document.getElementById("txt").value
+// }
+// diaries.push(diary);
+// // document.querySelector('form').reset();
+// document.forms[0].reset();
+
+// console.warn('added', {diaries});
+// let pre=document.querySelector('#msg pre');
+// pre.textContent = localStorage.getItem(JSON.stringify(diaries.titles)) + " has been added";
+
+// localStorage.setItem('JournalDocuments', JSON.stringify(diaries));
+
+// }
+// document.addEventListener('DOMContentLoaded', ()=> {
+//     document.getElementById('createButton').addEventListener('click', addDiary);
+// })
+
+let addThis = "";
+let andThis = "";
+
+for (let i = 0; i < 10; i++) {
+addThis = i + "Olafs";
+andThis = i + "Newbs";
+// const createValue = localStorage.setItem(addThis, andThis);
+
 }
-diaries.push(diary);
-// document.querySelector('form').reset();
-document.forms[0].reset();
-
-console.warn('added', {diaries});
-let pre=document.querySelector('#msg pre');
-pre.textContent = diary.titles + " has been added";
-
-const todaysToString = diary.id.toString();
-const todaysDate = todaysToString.slice(0, 15);
-const returnJournal = diary.titles + " || " + todaysDate;
-document.getElementById('listOfJournals').innerHTML = returnJournal;
-
-localStorage.setItem('JournalDocuments', JSON.stringify(diaries));
-}
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById('createButton').addEventListener('click', addDiary);
-})
+let listOf = document.getElementById('listOfJournals');
+listOf.textContent = localStorage.key(1);
