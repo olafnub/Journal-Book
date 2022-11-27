13b63c1ca2df62e385ed35f91450b11e3295a813
olafnub <googlhoogle@gmail.com>
2022-11-26 20:05:52 -0600

    Polish submission log and create Your journals tab
    **Next commit - hopefully fix added journals to be on screen even after
    refreshed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let diaries = [];
 const addDiary = (ev)=>{
 ev.preventDefault();
     let diary = {
-    id: Date.now(),
+    id: Date(Date.now()),
     titles: document.getElementById("diaryTitle").value,
     paragraphs: document.getElementById("txt").value
 }
@@ -14,6 +14,11 @@ document.forms[0].reset();
 console.warn('added', {diaries});
 let pre=document.querySelector('#msg pre');
 pre.textContent = diary.titles + " has been added";
+
+const todaysToString = diary.id.toString();
+const todaysDate = todaysToString.slice(0, 15);
+const returnJournal = diary.titles + " || " + todaysDate;
+document.getElementById('listOfJournals').innerHTML = returnJournal;
 
 localStorage.setItem('JournalDocuments', JSON.stringify(diaries));
 }
