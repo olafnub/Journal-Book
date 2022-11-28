2f254c733facf9ee54a6e1ae40b2401457133ad9
olafnub <googlhoogle@gmail.com>
2022-11-27 21:23:46 -0600

    Fix submission log

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ ev.preventDefault();
     paragraphs: document.getElementById("txt").value
 }
 diaries.push(diary);
-// document.querySelector('form').reset();
 document.forms[0].reset();
 
+// Submission Log
 console.warn('added', {diaries});
 let pre=document.querySelector('#msg pre');
-pre.textContent = localStorage.getItem(JSON.stringify(diaries.titles)) + " has been added";
+pre.textContent = JSON.stringify(diary.titles) + " has been added";
 
+
+ // If there are two of the same titles
+ let repeatingTitle = 0;
+for (let k = 0; k < localStorage.length; k++) {
+    
+if (diary.titles.toString() == localStorage.key(k).toString()) {
+    repeatingTitle++;
+    for (let i = 0; i < localStorage.length; i++) {
+        diary.titles = diary.titles + " " + repeatingTitle;
+    }
+}
+}
 localStorage.setItem(diary.titles, JSON.stringify(diaries));
 
 
@@ -23,3 +35,5 @@ document.addEventListener('DOMContentLoaded', ()=> {
     document.getElementById('createButton').addEventListener('click', addDiary);
 })
 
+let setJournalName = document.getElementById('listOfJournals').innerHTML;
+setJournalName = diary.id;
